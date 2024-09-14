d3670b1514894ed8b5076a140a0ce0e55c424e1e
Pwangui <127877940+WanPgui@users.noreply.github.com>
2024-09-14 16:40:06 +0200

    Update 2-size_me_please.py

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
+"""
+Module to calculate the shape of a matrix.
+"""
+
 def matrix_shape(matrix):
-    shape = [len(matrix)]
-    while isinstance(matrix[0], list):
-        shape.append(len(matrix[0]))
+    """
+    Returns the shape of a matrix as a list of integers.
+    
+    Args:
+        matrix (list): The matrix for which the shape is calculated.
+
+    Returns:
+        list: The shape of the matrix.
+    """
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
         matrix = matrix[0]
     return shape
