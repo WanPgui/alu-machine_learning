c6c2f0cace9cc9c5d30fc0138873794d5e722b9a
Pwangui <127877940+WanPgui@users.noreply.github.com>
2025-03-02 21:44:03 +0300

    Update 2-user_location.py

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-
-""" Return list of ships"""
+""" Return location info """
 
 import requests
 import sys
@@ -9,17 +8,25 @@ import time
 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python 2-user_location.py <url>")
+        sys.exit(1)
+
     res = requests.get(sys.argv[1])
 
     if res.status_code == 403:
-        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
+        rate_limit = int(res.headers.get('X-Ratelimit-Reset', 0))
         current_time = int(time.time())
         diff = (rate_limit - current_time) // 60
         print("Reset in {} min".format(diff))
-        # get remaining rate
 
     elif res.status_code == 404:
         print("Not found")
+
     elif res.status_code == 200:
         res = res.json()
-        print(res['location'])
+        print(f"IP: {res.get('ip')}")
+        print(f"City: {res.get('city')}")
+        print(f"Region: {res.get('region')}")
+        print(f"Country: {res.get('country')}")
+        print(f"Location (Lat, Long): {res.get('loc')}")
